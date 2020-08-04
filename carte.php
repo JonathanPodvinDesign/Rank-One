2f671711ceece7e2898c4965852fa00d04a2e932
Jonathan Design <jonathanpodvindesign@gmail.com>
2020-08-04 22:12:14 +0200

    {Dev}[Carte](Create carte page)

--- carte.php
+++ carte.php
@@ -54,42 +54,142 @@
       </div>
       <!-- carte -->
       <div class="carte__categories">
-        <div class="carte__categories__category">
-          <div class="carte__categories__category__title">
-            <h6>Alcools</h6>
-          </div>
-          <div class="carte__categories__category__boissons">
-            <div>
-              <p>Bière</p>
-              <p>Pinte</p>
-              <p>Whisky</p>
-              <p>Vodka</p>
-              <p>Bouteille</p>
+        <!-- First Column -->
+        <div class="carte__categories__col">
+          <!-- alcools -->
+          <div class="carte__categories__col__boissons">
+            <div class="carte__categories__col__boissons__title">
+              <h6>Alcools</h6>
             </div>
-            <div>
-              <p>4,00€</p>
-              <p>6,30€</p>
-              <p>4,00€</p>
-              <p>4,00€</p>
-              <p>70,00€</p>
+            <div class="carte__categories__col__boissons__names">
+              <div>
+                <p>bière</p>
+                <p>pinte</p>
+                <p>whisky</p>
+                <p>vodka</p>
+                <p>bouteille</p>
+              </div>
+              <div>
+                <p>4,00€</p>
+                <p>6,30€</p>
+                <p>4,00€</p>
+                <p>4,00€</p>
+                <p>70,00€</p>
+              </div>
+            </div>
+          </div>
+          <!-- cocktails -->
+          <div class="carte__categories__col__boissons">
+            <div class="carte__categories__col__boissons__title">
+              <h6>Cocktails</h6>
+            </div>
+            <div class="carte__categories__col__boissons__names">
+              <div>
+                <p>margarita</p>
+                <p>pina coloda</p>
+                <p>mojito</p>
+                <p>ti'punch</p>
+                <p>sex on the beach</p>
+                <p>tequila sunrise</p>
+              </div>
+              <div>
+                <p>6,80€</p>
+                <p>6,80€</p>
+                <p>6,80€</p>
+                <p>6,80€</p>
+                <p>6,80€</p>
+                <p>6,80€</p>
+              </div>
+            </div>
+          </div>
+          <!-- Boissons chaudes -->
+          <div class="carte__categories__col__boissons">
+            <div class="carte__categories__col__boissons__title">
+              <h6>Boissons chaudes</h6>
+            </div>
+            <div class="carte__categories__col__boissons__names">
+              <div>
+                <p>café</p>
+                <p>capuccino</p>
+                <p>doublé café</p>
+                <p>chocolat</p>
+              </div>
+              <div>
+                <p>2,50€</p>
+                <p>1,80€</p>
+                <p>3,00€</p>
+                <p>1,50€</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Second Column -->
+        <div class="carte__categories carte__categories__col">
+          <!-- Boissons -->
+          <div class="carte__categories__col__boissons">
+            <div class="carte__categories__col__boissons__title">
+              <h6>Boissons</h6>
+            </div>
+            <div class="carte__categories__col__boissons__names">
+              <div>
+                <p>coca-cola</p>
+                <p>limonade</p>
+                <p>sirop</p>
+                <p>jus de fruit</p>
+                <p>redbull</p>
+                <p>perrier</p>
+                <p>eau minérale</p>
+              </div>
+              <div>
+                <p>3,00€</p>
+                <p>3,00€</p>
+                <p>2,00€</p>
+                <p>2,00€</p>
+                <p>3,00€</p>
+                <p>2,50€</p>
+                <p>2,30€</p>
+              </div>
+            </div>
+          </div>
+          <!-- cocktails sans alcool -->
+          <div class="carte__categories__col__boissons">
+            <div class="carte__categories__col__boissons__title">
+              <h6>Cocktails sans alcool</h6>
+            </div>
+            <div class="carte__categories__col__boissons__names">
+              <div>
+                <p>virgin mojito</p>
+                <p>bora bora</p>
+                <p>after glow</p>
+                <p>bella luna</p>
+                <p>fruits cup</p>
+                <p>florida</p>
+              </div>
+              <div>
+                <p>6,50€</p>
+                <p>6,50€</p>
+                <p>6,50€</p>
+                <p>6,50€</p>
+                <p>6,50€</p>
+                <p>6,50€</p>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-      
 
-  <!-- FOOTER -->
-  <!-- FOOTER -->
-  <!-- FOOTER -->
-  <?php
+    <!-- FOOTER -->
+    <!-- FOOTER -->
+    <!-- FOOTER -->
+    <?php
       include 'footer-social.php';
   ?>
 
-  <!-- SCRIPT -->
-  <!-- SCRIPT -->
-  <!-- SCRIPT -->
-  <?php
+    <!-- SCRIPT -->
+    <!-- SCRIPT -->
+    <!-- SCRIPT -->
+    <?php
       include 'script.php';
   ?>
   </body>
