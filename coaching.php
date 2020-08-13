0a084c9c2259913d0fc7887338223d7737bbdf16
Jonathan Design <jonathanpodvindesign@gmail.com>
2020-08-13 21:17:59 +0200

    {Dev}[coaching](Création page coaching)

---
+++ coaching.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <!-- HEAD -->
+
+  <head>
+    <title>Rank One - Coaching</title>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!-- CSS -->
+    <!-- CSS__fonts -->
+    <link
+      href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,400,400i,700,700i&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      href="https://fonts.googleapis.com/css?family=Graduate&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="assets/fonts/fonts.css" />
+    <!-- CSS__fontawesome -->
+    <link rel="stylesheet" href="assets/fontawesome/css/all.min.css" />
+    <!-- CSS__flexbos -->
+    <link rel="stylesheet" href="assets/flexbox/css/flexboxgrid.css" />
+    <!-- CSS__style -->
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+
+  <!-- BODY -->
+  <!-- BODY -->
+  <!-- BODY -->
+
+  <body>
+    <div class="background">
+      <div>&nbsp</div>
+      <div>&nbsp</div>
+      <div>&nbsp</div>
+      <div>&nbsp</div>
+    </div>
+    <!-- HEADER --------->
+    <!-- HEADER --------->
+    <!-- HEADER --------->
+    <?php
+      include 'header-yellow.php';
+  ?>
+
+    <div class="main margin-top-fixed-for-sticky-menu">
+      <!-- COACHING -->
+      <!-- COACHING -->
+      <!-- COACHING -->
+      <div class="coaching">
+        <!-- TITLE -->
+        <div class="categories__title categories__title--coaching">
+          <div class="hook">
+            <img src="img/hook-yellow.svg" alt="Hook" />
+          </div>
+          <h2>
+            Réserver<br />
+            un coach
+          </h2>
+        </div>
+
+        <!-- COACHING SELECTION -->
+        <div class="coaching__selection">
+          <div class="coaching__selection__games">
+            <p>Jeux</p>
+          </div>
+          <div class="coaching__selection__date">
+            <p>Dates</p>
+          </div>
+          <div class="coaching__selection__horaire">
+            <p>Horaires</p>
+          </div>
+        </div>
+
+        <!-- COACHING LIST -->
+        <div class="coaching__list">
+          <div class="coaching__list__jeux">
+            <div class="coaching__list__jeux__jeu"><p>Counter Strike</p></div>
+            <div class="coaching__list__jeux__jeu"><p>Dota 2</p></div>
+            <div class="coaching__list__jeux__jeu"><p>Fifa</p></div>
+            <div class="coaching__list__jeux__jeu"><p>Fortnite</p></div>
+            <div class="coaching__list__jeux__jeu"><p>Hearthstone</p></div>
+            <div class="coaching__list__jeux__jeu">
+              <p>League of legends</p>
+            </div>
+            <div class="coaching__list__jeux__jeu"><p>Overwatch</p></div>
+            <div class="coaching__list__jeux__jeu"><p>Valorant</p></div>
+          </div>
+          <div class="coaching__list__dates">
+            <div class="coaching__list__dates__date">
+              <i
+                class="fas fa-arrow-left coaching__list__dates__date__arrow"
+              ></i
+              >Semaine<i
+                class="fas fa-arrow-right coaching__list__dates__date__arrow"
+              ></i>
+            </div>
+            <div class="coaching__list__dates__date">
+              <p>02 Juillet 2018</p>
+            </div>
+            <div class="coaching__list__dates__date">
+              <p>03 Juillet 2018</p>
+            </div>
+            <div class="coaching__list__dates__date">
+              <p>04 Juillet 2018</p>
+            </div>
+            <div class="coaching__list__dates__date">
+              <p>05 Juillet 2018</p>
+            </div>
+            <div class="coaching__list__dates__date">
+              <p>06 Juillet 2018</p>
+            </div>
+            <div class="coaching__list__dates__date">
+              <p>07 Juillet 2018</p>
+            </div>
+            <div class="coaching__list__dates__date">
+              <p>08 Juillet 2018</p>
+            </div>
+          </div>
+          <div class="coaching__list__horaire">
+            <div class="coaching__list__horaire__heure"><p>17h à 18h</p></div>
+            <div class="coaching__list__horaire__heure"><p>18h à 19h</p></div>
+            <div class="coaching__list__horaire__heure"><p>19h à 20h</p></div>
+            <div class="coaching__list__horaire__heure"><p>20h à 21h</p></div>
+            <div class="coaching__list__horaire__heure"><p>21h à 22h</p></div>
+            <div class="coaching__list__horaire__heure"><p>22h à 23h</p></div>
+            <div class="coaching__list__horaire__heure"><p>23h à 00h</p></div>
+            <div class="coaching__list__horaire__heure"><p>00h à 01h</p></div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Div Fix Small Height Page -->
+    <div class="fix__small__height__page"></div>
+
+    <!-- FOOTER -->
+    <!-- FOOTER -->
+    <!-- FOOTER -->
+    <?php
+      include 'footer-social.php';
+  ?>
+
+    <!-- SCRIPT -->
+    <!-- SCRIPT -->
+    <!-- SCRIPT -->
+    <?php
+      include 'script.php';
+  ?>
+  </body>
+</html>
