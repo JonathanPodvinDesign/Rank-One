5ad3febf06613ee24f59850edbd7d97ce34c51fc
Jonathan Design <jonathanpodvindesign@gmail.com>
2020-08-16 09:48:55 +0200

    {Fix}[Header & titre pages](hover header et titre des pages)

--- blog.php
+++ blog.php
@@ -3,7 +3,7 @@
   <!-- HEAD -->
 
   <head>
-    <title>Rank One - Contact</title>
+    <title>Rank One - Blog</title>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
--- product.php
+++ product.php
@@ -3,7 +3,7 @@
   <!-- HEAD -->
 
   <head>
-    <title>Rank One - Product</title>
+    <title>Rank One - Produit</title>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
